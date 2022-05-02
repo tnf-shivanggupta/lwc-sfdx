ec175e2adf7f9bc6225cf5ab06929d5526b805a9
guptashi <95851691+guptshi@users.noreply.github.com>
2022-05-02 13:31:37 +0530

    init

---
+++ force-app/main/default/lwc/bikeCard/bikeCard.js
@@ -0,0 +1,10 @@
+import { LightningElement, api } from 'lwc';
+export default class BikeCard extends LightningElement {
+   @api 
+	 name;
+   description = 'A sweet bike built for comfort.';
+   category = 'Mountain';
+   material = 'Steel';
+   price = '$2,700';
+   pictureUrl = 'https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg';
+ }
