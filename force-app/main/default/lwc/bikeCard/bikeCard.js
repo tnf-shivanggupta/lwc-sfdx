a2e761e1a92f9db6534b72e49616422393994a41
guptashi <95851691+guptshi@users.noreply.github.com>
2022-05-05 13:56:29 +0530

    external svelte component in local-dev-server

--- force-app/main/default/lwc/bikeCard/bikeCard.js
+++ force-app/main/default/lwc/bikeCard/bikeCard.js
@@ -1,10 +1,27 @@
 import { LightningElement, api } from 'lwc';
+import Crud from '@salesforce/resourceUrl/crudapp';
+import { loadScript } from 'lightning/platformResourceLoader';
 export default class BikeCard extends LightningElement {
    @api 
 	 name;
    description = 'A sweet bike built for comfort.';
    category = 'Mountain';
-   material = 'Steel';
+   material = 'Steels';
    price = '$2,700';
    pictureUrl = 'https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg';
+	 attachmentPoint = null
+	 scriptLoaded = false;
+	 renderedCallback(){
+		 if(this.scriptLoaded){
+			 return
+		 }
+		// loadScript(this, "https://test-3bd-dev-ed.lightning.force.com/resource/1651727161000/crudapp/Crud.js").then(() => {
+		loadScript(this, Crud + ".js").then(() => {
+			this.scriptLoaded = true;
+			const testDiv =this.template.querySelector('div[data-id=crudElement]');
+			testDiv.innerHTML = "<crud-element></crud-element>"
+		}).catch((e) => {
+			console.log(e)
+		})
+	 }
  }
